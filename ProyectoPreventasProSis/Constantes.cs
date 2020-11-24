f2d8f769ab47c6f35f8fd4cf264d7a49b1d5fd49
Luis Bone <lbone171853@gmail.com>
2020-11-24 16:08:11 -0500

    Cambios etiqueta

--- ProyectoPreventasProSis/Constantes.cs
+++ ProyectoPreventasProSis/Constantes.cs
@@ -6,7 +6,7 @@ namespace ProyectoPreventasProSis
 {
     class Constantes
     {
-        //IP SERVIDOR O MAQUINA LOCAL
+        //IP SERVIDOR O MAQUINA LOCAL 1
         public string servidor = "http://192.168.1.4/API/";
     }
 }
